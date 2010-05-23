121bcabfc968b5a976143b7b727c27124001aa1b
Brandon Wiley <brandon@stratus.ischool.utexas.edu>
2010-05-22 19:25:57 -0500

    Removed unnecessary __init__()

--- py/extensions/lite/lite_socket.py
+++ py/extensions/lite/lite_socket.py
@@ -3,20 +3,16 @@ from core.dust_socket import dust_socket
 from core.util import encodeAddress, xor
 
 class lite_socket(dust_socket):
-  def __init__(self, keys):
-    dust_socket.__init__(self, keys)
-    
-  def makeSession(self, address, tryInvite):    
+  def makeSession(self, address, tryInvite):
     addressKey=encodeAddress(address)
     if addressKey in self.sessionKeys:
-      return self.sessionKeys[addressKey]    
+      return self.sessionKeys[addressKey]
 
     h1=hash(addressKey.encode('ascii'))
     h2=hash(self.myAddressKey.encode('ascii'))
-    
-    sessionKey=xor(h1, h2)    
-    
+
+    sessionKey=xor(h1, h2)
+
     self.sessionKeys[addressKey]=sessionKey
     print('SessionKey:', len(self.sessionKeys[addressKey]))
     return sessionKey
-    
