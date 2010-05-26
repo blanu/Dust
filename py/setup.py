66f52663d5f6a1cdc675c424614c1c942e664ab3
Brandon Wiley <dust@blanu.net>
2010-05-26 00:21:21 -0500

    Added a setup.py in an attempt to make a .egg for distribution

---
+++ py/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name='Dust',
+  version='0.1',
+  description='Blocking-resistant network protocol',
+  author='Brandon Wiley',
+  author_email='dust@blanu.net',
+  url='http://github.com/blanu/Dust',
+  packages=['Dust'],
+  long_description="""\
+    Dust is an Internet protocol designed to resist a number of attacks currently in active use to censor Internet communication.
+  """,
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Environment :: No Input/Output (Daemon)",
+    "License :: OSI Approved :: BSD License",
+    "Programming Language :: Python",
+    "Intended Audience :: Developers",
+    "Operating System :: OS Independent",
+    "Topic :: Internet",
+    "Topic :: Security :: Cryptography",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+  ],
+  keywords='cryptography privacy internet',
+  license='BSD',
+  install_requires=[
+    'pyskein',
+    'bitstring',
+    'yaml',
+  ],
+)
