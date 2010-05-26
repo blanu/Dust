76dcc482e41e22a1377fbfec9771cdcf14f1fef0
Brandon Wiley <dust@blanu.net>
2010-05-26 00:24:51 -0500

    Added onion extension for dust packets inside of dust packets

---
+++ py/extensions/onion/onion_packet.py
@@ -0,0 +1,28 @@
+from core.data_packet import DataPacket
+from core.util import splitFields
+
+SENDER_LENGTH=32
+RECEIVER_LENGTH=32
+
+class OnionPacket(DataPacket):
+  def __init__(self):
+    DataPacket.__init__(self)
+
+    self.sender=None
+    self.receiver=None
+
+  def createOnionPacket(self, keypair, receiver, data, entropy):
+    self.sender=keypair.public.bytes
+    self.receiver=receiver
+    sk=keypair.createSession(Key(self.receiver, False))
+    self.createDataPacket(sk, data, entropy)
+    self.packet=self.sender+self.receiver+self.packet
+
+  def decodeOnionPacket(self, keypair, packet):
+    self.sender, self.receiver, packet=splitFields(packet, [SENDER_LENGTH, RECEIVER_LENGTH])
+    if keypair.public.bytes!=send.receiver:
+      print('Error! Onion packet meant for a different receiver. Keypair does not match')
+      return
+    else:
+      sk=keypair.createSession(Key(self.sender, False))
+      self.decodeDataPacket(sk, packet)
---
+++ py/extensions/onion/onion_socket.py
@@ -0,0 +1,78 @@
+import sys
+import time
+import struct
+from socket import *
+
+import yaml
+
+from crypto.curve import *
+from extensions.onion.onion_packet import OnionPacket
+from core.util import encodeAddress, encode
+from intro.intro import Introducer
+
+class onion_socket:
+  def __init__(self, keys, socket):
+    self.keys=keys
+    self.keypair=keys.getKeypair()
+    self.sock=socket
+
+    self.sessionKeys={}
+
+    self.remaining=None
+
+  def recvfrom(self, bufsize):
+    if self.remaining:
+      data, addr, endpoint=self.remaining
+      self.remaining=None
+    else:
+      data, addr, endpoint=self.sock.recvfrom(bufsize)
+
+    if not data:
+      print('Onion: No data')
+      return None, None, None
+    else:
+      packet=self.decodeOnionPacket(data)
+      if not packet:
+        print('Onion: No packet')
+        return None, None, None
+      else:
+        #print('Packet:')
+        #print(packet)
+        if packet.remaining:
+          self.remaining=(packet.remaining, addr, packet.endpoint)
+        if type(packet)==OnionPacket:
+          return packet.data, addr, packet.endpoint
+        else:
+          print('Not a data packet')
+          return None, None, None
+
+  def sendto(self, data, addr, endpoint):
+    print('sendto '+str(endpoint))
+    packet=self.encodePacket(endpoint, data)
+    print('Sending')
+    print(packet)
+    self.sock.sendto(packet.packet, 0, addr)
+
+  def decodePacket(self, endpoint, data):
+    packet=OnionPacket()
+    packet.decodeOnionPacket(data)
+    if packet.checkMac() and packet.checkTimestamp():
+      return packet
+    else:
+      print('Integrity failed', packet.checkMac(), packet.checkTimestamp())
+      print(packet)
+      return None
+    else: # Must be an intro packet
+      print('Unknown address', endpoint)
+      if self.introducer:
+        intro=self.introducer.acceptIntroduction(data, endpoint)
+        if intro:
+          return intro
+        else:
+          return None
+
+  def encodePacket(self, endpoint, data):
+    packet=OnionPacket()
+    packet.createOnionPacket(self.keys.getKeypair(), endpoint, data, self.keys.entropy)
+    return packet
+
