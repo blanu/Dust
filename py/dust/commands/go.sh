d91171f43b1c4b211f4df322158de7667de4b3eb
Brandon Wiley <dust@blanu.net>
2010-06-01 13:18:48 -0500

    Changed scripts to use new directory structure

--- py/dust/commands/go.sh
+++ py/dust/commands/go.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH=.
-python3.1 commands/make-invite.py test 7000 6
+python3.1 dust/commands/make-invite.py test 7000 6
 #rm config/outgoing_invites.ip
-python3.1 commands/process-invite.py test test
-python3.1 commands/serve.py 7000 6 test [2001:0:53aa:64c:1474:74db:baa2:c211]:7040
+python3.1 dust/commands/process-invite.py test test
+python3.1 dust/commands/serve.py 7000 6 test [2001:0:53aa:64c:1474:74db:baa2:c211]:7040
--- py/dust/commands/tracker.sh
+++ py/dust/commands/tracker.sh
@@ -1,4 +1,4 @@
-python3.1 commands/make-invite.py test 7040 6
+python3.1 dust/commands/make-invite.py test 7040 6
 rm config/outgoing_invites.ip
-python3.1 commands/process-invite.py test test
-python3.1 commands/tracker-serve.py 7040 6 test
+python3.1 dust/commands/process-invite.py test test
+python3.1 dust/commands/tracker-serve.py 7040 6 test
