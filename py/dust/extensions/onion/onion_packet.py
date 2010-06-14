b709295e0321d5e8f15f6d3b3bef293e3dce7e1c
Brandon Wiley <dust@blanu.net>
2010-06-13 19:55:38 -0500

    Log more information on error

--- py/dust/extensions/onion/onion_packet.py
+++ py/dust/extensions/onion/onion_packet.py
@@ -1,5 +1,6 @@
+import logging
 from dust.core.data_packet import DataPacket
-from dust.core.util import splitFields
+from dust.core.util import splitFields, encode
 
 SENDER_LENGTH=32
 RECEIVER_LENGTH=32
@@ -21,7 +22,7 @@ class OnionPacket(DataPacket):
   def decodeOnionPacket(self, keypair, packet):
     self.sender, self.receiver, packet=splitFields(packet, [SENDER_LENGTH, RECEIVER_LENGTH])
     if keypair.public.bytes!=self.receiver:
-      print('Error! Onion packet meant for a different receiver. Keypair does not match')
+      print('Error! Onion packet meant for a different receiver. Keypair does not match: '+encode(self.sender)+' '+encode(self.receiver)+' '+encode(keypair.public.bytes))
       return
     else:
       skb=keypair.createSessionBytes(self.sender)
