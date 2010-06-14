fbaf2f237ad407b630b58151131b771244743b58
Brandon Wiley <dust@blanu.net>
2010-06-13 19:53:28 -0500

    Fixed Ctrl-C handling (again!)

--- py/dust/util/safethread.py
+++ py/dust/util/safethread.py
@@ -1,10 +1,15 @@
+import sys
 import traceback
 from threading import Thread, Event
 
 # If you don't do it this way, ctrl-c doesn't work anymore.
 def waitForEvent(event):
-  while not event.isSet():
-    event.wait(1000)
+  done=False
+  while not event.isSet() and not done:
+    try:
+      event.wait(1000)
+    except KeyboardInterrupt:
+      done=True
 
 def waitFor(delay):
 #  Event().wait(delay)
@@ -14,14 +19,31 @@ def waitFor(delay):
 def wait():
 #  Event().wait()
   import time
-  while True:
-    time.sleep(1000)
+  done=False
+  while not done:
+    try:
+      time.sleep(1000)
+    except KeyboardInterrupt:
+      print('Ctrl-C')
+      done=True
+
+def waitForThreads(threads):
+  while len(threads) > 0:
+    try:
+      print('Threads before: '+str(threads))
+      threads = [t.join(1) for t in threads if t is not None and t.isAlive()]
+      print('Threads after: '+str(threads))
+    except KeyboardInterrupt:
+      print("Ctrl-c received! Sending kill to threads...")
+      for t in threads:
+        t.done = True
 
 class SafeThread(Thread):
   def __init__(self, target=None, args=None):
+    Thread.__init__(self, target=self.safeCall)
     self.method=target
     self.args=args
-    Thread.__init__(self, target=self.safeCall)
+    self.done=False
 
   def safeCall(self):
     if self.method==None:
@@ -34,4 +56,3 @@ class SafeThread(Thread):
     except Exception:
       print('Internal crash')
       traceback.print_exc()
-
