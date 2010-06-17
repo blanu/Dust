a5af9c967065f4dfd43a98587597e6dde9e12bd7
Brandon Wiley <dust@blanu.net>
2010-06-16 23:10:57 -0500

    Print or save invite to file

--- py/dust/services/dustmail/invite.py
+++ py/dust/services/dustmail/invite.py
@@ -1,23 +1,25 @@
 import os
 import sys
 import time
+from threading import Event
 from dust.crypto.keys import KeyManager
 from dust.crypto.curve import Key
 from dust.core.util import getPublicIP, encode, decode, encodeAddress, decodeAddress
 from dust.server.router import PacketRouter
-from dust.util.safethread import wait
+from dust.util.safethread import waitForEvent
 from dust.invite.invite_packet import InviteMessage
 from dust.extensions.onion.onion_packet import OnionPacket
 
 from dust.services.tracker.trackerClient import TrackerClient
 from dust.services.dustmail.dustmailClient import DustmailClient
 from dust.invite.invite_packet import InviteMessage
+from dust.services.dustmail.dustmail_packet import DustmailInvitePacket
 
 passwd=sys.argv[1]
 inport=int(sys.argv[2])
 destAddress=sys.argv[3]
 dest, outport, v6=decodeAddress(destAddress)
-recipient=sys.argv[4]
+ipasswd=sys.argv[4]
 
 host=getPublicIP(v6)
 print('Host: '+str(host))
@@ -34,10 +36,25 @@ keypair=keys.getKeypair()
 pubkey=keypair.public
 pubkeyhex=encode(pubkey.bytes)
 
-destpubkey=Key(decode(recipient), False)
+done=Event()
 
 def gotInvite(invite):
-  print('gotInvite('+str(invite)+')')
+  ps=input("Print or Save [P/s]?")
+  passwd=input("Encrypt invite with password: ")
+  packet=DustmailInvitePacket()
+  packet.createDustmailInvitePacket(passwd, endpoint.public.bytes, invite, keys.entropy)
+  if ps=='s':
+    filename=input("Save invite to filename: ").strip()
+    if filename!='':
+      f=open(filename, 'wb')
+      f.write(packet.packet)
+      f.close()
+  else:
+    print()
+    print(encode(packet.packet))
+    print()
+
+  done.set()
 
 router=PacketRouter(v6, inport, keys, passwd)
 router.connect(dest, outport)
@@ -47,7 +64,7 @@ trackback=router.getService('trackback')
 
 router.start()
 
-trackback.setPutTrackerInvite(self.gotInvite)
+trackback.setPutTrackerInviteCallback(gotInvite)
 tracker.getTrackerInvite()
 
-wait()
+waitForEvent(done)
