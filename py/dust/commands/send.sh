27274564af29dca10c77ef5242c8ee459b0fbe9f
Brandon Wiley <dust@blanu.net>
2010-06-16 20:11:39 -0500

    Changed recipient id

--- py/dust/commands/send.sh
+++ py/dust/commands/send.sh
@@ -1,1 +1,1 @@
-python3.1 dust/services/dustmail/send.py test 7002 [2001:470:1f0e:63a::2]:7040 90f1fbef8a2c9890751b2cdad1898ee13f7037becf70a1178c5da2fea3879f64 "$1"
+python3.1 dust/services/dustmail/send.py test 7002 [2001:470:1f0e:63a::2]:7040 7d0aeb009a71496de7be1753cfcd8797f33ee81c3d8908416588755542355829 "$1"
