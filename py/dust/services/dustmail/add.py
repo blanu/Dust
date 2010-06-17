33a3aeba3530b872273949d6d6e085fa2cbf07b6
Brandon Wiley <dust@blanu.net>
2010-06-16 23:12:31 -0500

    Command to read a dustmail invite

---
+++ py/dust/services/dustmail/add.py
@@ -0,0 +1,41 @@
+import os
+import sys
+from dust.crypto.keys import KeyManager
+from dust.crypto.curve import Key
+from dust.core.util import getPublicIP, encode, decode, encodeAddress, decodeAddress
+from dust.util.ymap import YamlMap
+
+from dust.invite.invite_packet import InviteMessage
+from dust.services.dustmail.dustmail_packet import DustmailInvitePacket
+
+passwd=sys.argv[1]
+
+keys=KeyManager()
+keys.setInvitePassword(passwd)
+keys.loadKnownHosts('config/knownhosts.yaml')
+keys.loadKeypair('config/id.yaml')
+keys.loadIncomingInvites('config/incoming_invites.ip')
+keys.loadOutgoingInvites('config/outgoing_invites.ip')
+endpoint=keys.loadEndpoint(os.path.expanduser('~/.dust/endpoint.yaml'))
+
+pf=input("Load invite from Paste or File [P/f]? ")
+if pf=='f':
+  filename=input("Load invite from filename: ").strip()
+  f=open(filename, 'rb')
+  data=f.read()
+  f.close()
+else:
+  data=decode(input("Past invite: "))
+
+passwd=input("Decrypt invite with password: ")
+packet=DustmailInvitePacket()
+packet.decodeDustmailInvitePacket(passwd, data)
+print("pubkey: "+encode(packet.pubkey))
+print("invite: "+encode(packet.invite))
+invite=InviteMessage()
+invite.decodeInviteMessage(packet.invite)
+keys.addInvite(invite)
+
+name=input("Name for this endpoint: ")
+book=YamlMap('config/dustmail-addressbook.yaml')
+book[encode(packet.pubkey)]=name
