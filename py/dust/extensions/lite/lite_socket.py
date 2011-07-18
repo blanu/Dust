2ee2dd21b7129e6e34ecfb8934c578b6065172e4
Brandon Wiley <brandon@blanu.net>
2011-07-18 12:38:43 -0500

    Debug print

--- py/dust/extensions/lite/lite_socket.py
+++ py/dust/extensions/lite/lite_socket.py
@@ -1,6 +1,6 @@
 from dust.crypto.dustUtil import hash
 from dust.core.dust_socket import dust_socket
-from dust.core.util import encodeAddress, xor
+from dust.core.util import encodeAddress, xor, encode
 
 class lite_socket(dust_socket):
   def makeSession(self, address, tryInvite):
@@ -10,10 +10,12 @@ class lite_socket(dust_socket):
       return self.sessionKeys[addressKey]
 
     h1=hash(addressKey.encode('ascii'))
+    print('h1: '+str(encode(h1)))
     h2=hash(self.myAddressKey.encode('ascii'))
+    print('h2: '+str(encode(h2)))
 
     sessionKey=xor(h1, h2)
-    print('Lite Session Key: '+str(sessionKey))
+    print('Lite Session Key: '+str(encode(sessionKey)))
 
     self.sessionKeys[addressKey]=sessionKey
     print('SessionKey:', len(self.sessionKeys[addressKey]))
