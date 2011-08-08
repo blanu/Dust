d62ad1dd35702cc8b1c465de6e177c04aa0592db
Brandon Wiley <brandon@blanu.net>
2011-08-08 15:03:28 -0500

    Eliminate Dust Lite encryption of session keys

--- py/dust/services/socks2/dustSocksServer.py
+++ py/dust/services/socks2/dustSocksServer.py
@@ -23,18 +23,11 @@ from socks import *
 @_o
 def handle_dust(conn):
   print('handle_dust')
-  myAddr=conn._stack_conn.iostream.socket.getsockname()
-  dest=conn._stack_conn.iostream.socket.getpeername()
-
-  sessionKey=makeSession(myAddr, dest)
-  coder=lite_socket(sessionKey)
-
   coder=yield handshake(coder, conn)
 
-  buffer=FakeSocket()
-
   client = Client()
-  yield client.connect('localhost', 9050)
+#  yield client.connect('localhost', 9050)
+  yield client.connect('blanu.net', 80)
 
   monocle.launch(pump, conn, client, coder.decrypt)
   yield pump(client, conn, coder.encrypt)
@@ -42,7 +35,6 @@ def handle_dust(conn):
 @_o
 def handshake(coder, conn):
   ivIn=yield conn.read(IV_SIZE)
-  coder.setIVIn(ivIn)
 
   yield conn.write(coder.ivOut)
 
@@ -52,10 +44,10 @@ def handshake(coder, conn):
   epub=yield conn.read(KEY_SIZE)
 
   esession=makeEphemeralSession(ekeypair, epub).bytes
-  newCoder=lite_socket(esession, ivIn=coder.ivIn, ivOut=coder.ivOut)
+  coder=lite_socket(esession, ivIn=coder.ivIn, ivOut=coder.ivOut)
 
 #  yield Return(coder)
-  yield Return(newCoder)
+  yield Return(coder)
 
 add_service(Service(handle_dust, port=7051))
 eventloop.run()
