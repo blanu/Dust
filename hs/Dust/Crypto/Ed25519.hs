3badbf55be9522fa6722db8545695e4332a3e675
Stephen Paul Weber <singpolyma@singpolyma.net>
2013-08-05 14:45:28 -0500

    Refactor Ed25519 bindings to use safer functions
    No more rooting around in the .Internal guts of ByteString, and
    especially no more unsafely packing a buffer than might be about to get
    garbage collected.
    
    This revision still uses unsafeUseAsCString instead of malloc, because
    it saves us a copy (since otherwise we would need to allocate the
    buffer, then copy it into the ByteString).  This should work, but care
    must be taken when changing these functions in the future.  Comments
    have been inserted to call out the assumptions.
    
    Also, the C code uses unsigned char*, whereas on the Haskell side I am
    using CString = Ptr CChar.  This should be safe if we just treat the
    data as bytes, but I have inserted a comment about this assumption as
    well.

--- hs/Dust/Crypto/Ed25519.hs
+++ hs/Dust/Crypto/Ed25519.hs
@@ -10,65 +10,64 @@ module Dust.Crypto.Ed25519
 
 import qualified Data.ByteString as B
 import Data.ByteString (ByteString)
-import Data.ByteString.Char8 (pack)
-import Data.ByteString.Internal (toForeignPtr)
-import Data.ByteString.Unsafe (unsafePackCStringFinalizer)
-
-import System.IO.Unsafe (unsafePerformIO)
+import Data.ByteString.Unsafe (unsafeUseAsCString)
 
 import Foreign.C
-import Foreign.Ptr
-import Foreign.ForeignPtr (withForeignPtr)
-import Data.Word
-
-type WordSize = Word64 --Set to Word32 or Word64 depending on 32-bit or 64-bit platform
---type WordSize = Word32 --Set to Word32 or Word64 depending on 32-bit or 64-bit platform
-
-withByteStringPtr :: ByteString -> (Ptr Word8 -> IO a) -> IO a
-withByteStringPtr b f =
-    withForeignPtr fptr $ \ptr -> f (ptr `plusPtr` off)
-    where (fptr, off, _) = toForeignPtr b
+import Foreign.Marshal.Unsafe (unsafeLocalState)
 
 ed25519_publickey :: ByteString -> ByteString
-ed25519_publickey bs = unsafePerformIO $ unsafe_ed25519_publickey bs
+ed25519_publickey = unsafeLocalState . unsafe_ed25519_publickey
 
 ed25519_sign_open :: ByteString -> ByteString -> ByteString -> Bool
-ed25519_sign_open msg pubkey signature = unsafePerformIO $ unsafe_ed25519_sign_open msg pubkey signature
+ed25519_sign_open msg pubkey signature = unsafeLocalState $ unsafe_ed25519_sign_open msg pubkey signature
 
 ed25519_sign :: ByteString -> ByteString -> ByteString -> ByteString
-ed25519_sign msg secret pubkey = unsafePerformIO $ unsafe_ed25519_sign msg secret pubkey
+ed25519_sign msg secret pubkey = unsafeLocalState $ unsafe_ed25519_sign msg secret pubkey
 
 unsafe_ed25519_publickey :: ByteString -> IO ByteString
-unsafe_ed25519_publickey input = do
-    withByteStringPtr (pack "1234567890123457890123456789012") $ \output -> do
-        withByteStringPtr input $ \cinput -> do
+unsafe_ed25519_publickey input =
+    -- This ByteString will be overwritten by the C call, but so long as the C
+    -- side does not keep a reference to it afterward, it does what we expect
+    -- and saves us a copy
+    unsafeUseAsCString outBS $ \output ->
+        B.useAsCString input $ \cinput -> do
             c_ed25519_publickey cinput output
-            unsafePackCStringFinalizer output 32 emptyFinalizer
+            return outBS
+    where
+    outBS = B.replicate 32 0xAB
+    {-# NOINLINE outBS #-}
 
 unsafe_ed25519_sign_open :: ByteString -> ByteString -> ByteString -> IO Bool
-unsafe_ed25519_sign_open msg pubkey signature = do
-    withByteStringPtr msg $ \cmsg -> do
-        withByteStringPtr pubkey $ \cpubkey -> do
-            withByteStringPtr signature $ \csignature -> do
-                let msgSize = CSize ((fromIntegral $ B.length msg)::WordSize)
-                result <- c_ed25519_sign_open cmsg msgSize cpubkey csignature
-                case result of
-                    0 -> return (True)
-                    otherwise -> return (False)
+unsafe_ed25519_sign_open msg pubkey signature =
+    B.useAsCStringLen msg $ \(cmsg, cmsglen) ->
+        B.useAsCString pubkey $ \cpubkey ->
+            B.useAsCString signature $ \csignature ->
+                let result = c_ed25519_sign_open cmsg (fromIntegral cmsglen) cpubkey csignature in
+                    return (result == 0)
 
 unsafe_ed25519_sign :: ByteString -> ByteString -> ByteString -> IO ByteString
-unsafe_ed25519_sign msg secret pubkey = do
-    withByteStringPtr (pack "12345678901234578901234567890123456789012345789012345678901234") $ \signature -> do
-        withByteStringPtr msg $ \cmsg -> do
-            withByteStringPtr secret $ \csecret -> do
-                withByteStringPtr pubkey $ \cpubkey -> do
-                    let msgSize = CSize ((fromIntegral $ B.length msg)::WordSize)
-                    c_ed25519_sign cmsg msgSize csecret cpubkey signature
-                    unsafePackCStringFinalizer signature 64 emptyFinalizer
+unsafe_ed25519_sign msg secret pubkey =
+    -- This ByteString will be overwritten by the C call, but so long as the C
+    -- side does not keep a reference to it afterward, it does what we expect
+    -- and saves us a copy
+    unsafeUseAsCString outBS $ \signature ->
+        B.useAsCStringLen msg $ \(cmsg, cmsglen) ->
+            B.useAsCString secret $ \csecret ->
+                B.useAsCString pubkey $ \cpubkey -> do
+                    c_ed25519_sign cmsg (fromIntegral cmsglen) csecret cpubkey signature
+                    return outBS
+    where
+    outBS = B.replicate 64 0xAB
+    {-# NOINLINE outBS #-}
 
-emptyFinalizer :: IO()
-emptyFinalizer = return ()
+-- Should be Ptr CUChar (not CString), but this should be safe if we just
+-- use them as bytes
 
-foreign import ccall unsafe "ed25519_publickey" c_ed25519_publickey :: Ptr Word8 -> Ptr Word8 -> IO ()
-foreign import ccall unsafe "ed25519_sign_open" c_ed25519_sign_open :: Ptr Word8 -> CSize -> Ptr Word8 -> Ptr Word8 -> IO (Int)
-foreign import ccall unsafe "ed25519_sign" c_ed25519_sign :: Ptr Word8 -> CSize -> Ptr Word8 -> Ptr Word8 -> Ptr Word8 -> IO ()
+foreign import ccall unsafe "ed25519_publickey" c_ed25519_publickey ::
+   CString -> CString -> IO ()
+
+foreign import ccall unsafe "ed25519_sign_open" c_ed25519_sign_open ::
+    CString -> CSize -> CString -> CString -> CInt
+
+foreign import ccall unsafe "ed25519_sign" c_ed25519_sign ::
+    CString -> CSize -> CString -> CString -> CString -> IO ()
