69780da784a7afabafbf58da9f4202c0546c04aa
Stephen Paul Weber <singpolyma@singpolyma.net>
2013-08-07 17:58:19 -0500

    Use safer functions for Curve25519
    Similar to the refactor for Ed25519.  The same one unsafe operation to
    save a copy.  The same use of CString even though the C side is actually
    uint8* in this case.  The same addition of size checks on the Haskell
    side.

--- hs/Dust/Crypto/Curve25519.hs
+++ hs/Dust/Crypto/Curve25519.hs
@@ -1,42 +1,38 @@
 {-# CFILES lib/curve25519-donna.c #-}
 {-# LANGUAGE ForeignFunctionInterface #-}
 
-module Dust.Crypto.Curve25519
-(
-  c_curve25519_donna,
-  unsafe_curve25519,
-  curve25519
-) where
+module Dust.Crypto.Curve25519 (curve25519) where
 
 import Data.ByteString (ByteString)
-import Data.ByteString.Char8 (pack)
-import Data.ByteString.Internal (toForeignPtr)
-import Data.ByteString.Unsafe (unsafePackCStringFinalizer)
+import qualified Data.ByteString as B
+import Data.ByteString.Unsafe (unsafeUseAsCString)
 
-import System.IO.Unsafe (unsafePerformIO)
-
-import Foreign.Ptr
-import Foreign.ForeignPtr (withForeignPtr)
-import Data.Word
-import Foreign.Marshal.Alloc (mallocBytes)
-
-withByteStringPtr :: ByteString -> (Ptr Word8 -> IO a) -> IO a
-withByteStringPtr b f =
-    withForeignPtr fptr $ \ptr -> f (ptr `plusPtr` off)
-    where (fptr, off, _) = toForeignPtr b
+import Foreign.Marshal.Unsafe (unsafeLocalState)
 
 curve25519 :: ByteString -> ByteString -> ByteString
-curve25519 bs1 bs2 = unsafePerformIO $ unsafe_curve25519 bs1 bs2
+curve25519 bs1 bs2 = curve
+	where
+	Just curve = unsafeLocalState $ unsafe_curve25519 bs1 bs2
 
-unsafe_curve25519 :: ByteString -> ByteString -> IO ByteString
-unsafe_curve25519 input1 input2 = do
-    output <- mallocBytes 32
-    withByteStringPtr input1 $ \cinput1 -> do
-        withByteStringPtr input2 $ \cinput2 -> do
-            result <- c_curve25519_donna output cinput1 cinput2
-            unsafePackCStringFinalizer output 32 emptyFinalizer
+unsafe_curve25519 :: ByteString -> ByteString -> IO (Maybe ByteString)
+unsafe_curve25519 secret basepoint
+    | B.length secret >= 32 && B.length basepoint >= 32 =
+        -- This ByteString will be overwritten by the C call, but so long as
+        -- the C side does not keep a reference to it afterward, it does what we
+        -- expect and saves us a copy
+        unsafeUseAsCString outBS $ \output ->
+            B.useAsCString input1 $ \csecret ->
+                B.useAsCString input2 $ \cbasepoint -> do
+                    result <- c_curve25519_donna output csecret cbasepoint
+                    case result of
+                        0 -> return outBS
+                        _ -> return Nothing
+    | otherwise = return Nothing
+    where
+    outBS = B.replicate 32 0xAB
 
-emptyFinalizer :: IO()
-emptyFinalizer = return ()
+-- Should be Ptr Word8 (not CString), but this should be safe if we just
+-- use them as bytes
 
-foreign import ccall unsafe "curve25519_donna" c_curve25519_donna :: Ptr Word8 -> Ptr Word8 -> Ptr Word8 -> IO (Int)
+foreign import ccall unsafe "curve25519_donna" c_curve25519_donna ::
+    CString -> CString -> CString -> IO Int
