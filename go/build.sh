e4a08879dc6768b4c23dbce9bdad8ea945fe9e05
Brandon Wiley <brandon@blanu.net>
2014-12-22 12:45:35 -0600

    gonum/stat is now included in the repo

--- go/build.sh
+++ go/build.sh
@@ -1,11 +1,7 @@
-if [ ! -d src/github.com/gonum/stat/ ];
-then
-  echo "Building stat"
-  go get github.com/gonum/stat
-  go build github.com/gonum/stat
-  go install github.com/gonum/stat
-  go test github.com/gonum/stat
-fi
+echo "Building stat"
+go build github.com/gonum/stat
+go install github.com/gonum/stat
+go test github.com/gonum/stat
 
 echo "Building Dust"
 go generate Dust
