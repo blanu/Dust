a622ee5b1ae300a1f5fefb7e0a26b96100c7b8db
Drake Wilson <drake@dasyatidae.com>
2015-01-16 20:08:58 -0600

    First cut of importing dchest's Skein
    This is actually Skein-512-256 rather than Skein-256-256, but
    blanu says this is okay---but this'll have to be documented in
    the "real" protocol doc.  The upstream library is MIT-ish
    license-wise.

--- go/DustCrypto/digest.go
+++ go/DustCrypto/digest.go
@@ -1,56 +1,29 @@
 package DustCrypto
 
 import (
-	cryptoSubtle "crypto/subtle"
+	"github.com/dchest/skein"
 )
 
-// INCOMPLETE (entire file): there's seemingly no Skein-256-256 available right now, so do something silly.
-
 type Digest struct {
-	state [32]byte
+	*skein.Hash
 }
 
-func NewDigest() *Digest {
-	d := &Digest{}
-	d.Reset()
-	return d
+func NewDigest() Digest {
+	return Digest{skein.New(32, nil)}
 }
 
-func (d *Digest) Reset() {
-	for i := 0; i < 32; i++ {
-		d.state[i] = 0
+func (d Digest) WriteSecret(sb SecretBytes) {
+	d.Hash.Write(sb.Slice())
+}
+
+func (d Digest) SumSecret(sbOut SecretBytes) {
+	n, err := d.Hash.OutputReader().Read(sbOut.Slice())
+	if n != 32 || err != nil {
+		panic("pure Skein should never fail a read")
 	}
 }
 
-func (d *Digest) WriteSecret(sb SecretBytes) {
-	_, _ = d.Write(sb.Slice())
-}
-
-func (d *Digest) Write(p []byte) (int, error) {
-	for i, b := range p {
-		d.state[i & 31] ^= b
-	}
-
-	return len(p), nil
-}
-
-func (d *Digest) SumSecret(sbOut SecretBytes) {
-	copy(sbOut.Slice(), d.state[:])
-}
-
-func (d *Digest) Sum(b []byte) []byte {
-	return append(b, d.state[:]...)
-}
-
-func (d *Digest) Size() int {
-	return 32
-}
-
-func (d *Digest) BlockSize() int {
-	return 32
-}
-
-func (d *Digest) Destroy() {
-	// Voodoo in the hopes that this will do the equivalent of explicit_bzero.
-	cryptoSubtle.ConstantTimeCopy(1, d.state[:], zeros[:])
+func (d Digest) Destroy() {
+	// TODO: Doesn't do anything right now, because the Skein doesn't provide this
+	// (but it's still unclear whether or not to just give up on this).
 }
--- go/DustCrypto/stream.go
+++ go/DustCrypto/stream.go
@@ -2,30 +2,11 @@ package DustCrypto
 
 import (
 	"crypto/cipher"
+
+	"github.com/dchest/skein"
 )
-
-// INCOMPLETE (entire file): where's the Skein?  Do something silly instead.
-
-type StreamCipher struct {
-	bogus [32]byte
-	i uint8
-}
 
 // NewStreamCipher takes a key and an IV and returns a stateful keystream.
 func NewStreamCipher(key SecretBytes, iv [32]byte) cipher.Stream {
-	sc := &StreamCipher{}
-	keyPtr := key.Pointer()
-	
-	for i := 0; i < 32; i++ {
-		sc.bogus[i] = keyPtr[i] ^ iv[i]
-	}
-
-	return sc
-}
-
-func (sc *StreamCipher) XORKeyStream(dst []byte, src []byte) {
-	for i, n := 0, len(src); i < n; i++ {
-		dst[i] = src[i] ^ sc.bogus[sc.i]
-		sc.i = (sc.i + 1) % 32
-	}
+	return skein.NewStream(key.Slice(), iv[:])
 }
