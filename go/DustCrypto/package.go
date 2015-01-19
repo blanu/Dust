fbea90d044c743cda296fac410d1b2836832eabd
Drake Wilson <drake@dasyatidae.com>
2015-01-18 18:31:10 -0600

    Add package comment to DustCrypto

---
+++ go/DustCrypto/package.go
@@ -0,0 +1,6 @@
+/*
+Package DustCrypto wraps the cryptographic primitives used by Dust.
+
+This package is mainly imported by the Dust package and is not intended for use by client applications directly.
+*/
+package DustCrypto
