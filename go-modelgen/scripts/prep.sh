2e3c1c24cdf8cdff30746683c345cf991fd0ef70
Brandon Wiley <brandon@blanu.net>
2015-02-04 13:59:39 -0600

    Install the logging dependency

--- go-modelgen/scripts/prep.sh
+++ go-modelgen/scripts/prep.sh
@@ -5,4 +5,5 @@ go get code.google.com/p/go.crypto/curve25519
 go get git.torproject.org/pluggable-transports/obfs4.git/common/csrand
 go get github.com/dchest/skein
 go get github.com/agl/ed25519/extra25519
+go get github.com/op/go-logging
 popd
