f9a8d82cad0d10c3ad92141f5031ead784b7d4e1
Brandon Wiley <brandon@blanu.net>
2015-02-04 11:14:09 -0600

    Run now uses DustProxy instead of dustexample

--- go-modelgen/run.sh
+++ go-modelgen/run.sh
@@ -1,3 +1,16 @@
-go build dustexample
-go install dustexample
-bin/dustexample
+./build.sh ../go/models caolta3iaejox3z4madc5wmp4z.uuid/Dust_privmodels ~/go/src/caolta3iaejox3z4madc5wmp4z.uuid/Dust_privmodels 
+
+pushd ~/go/src/github.com/blanu/Dust/go/cmd/DustTool
+go build
+go install
+rm ~/DustProxy.invite
+~/go/bin/DustTool newid -o ~/DustProxy.invite test 127.0.0.1:16667 m=http >~/DustProxy.bridge
+export DUSTPROXYID=`cat ~/DustProxy.bridge | awk '{print $4}'`
+popd
+
+pushd ~/go/src/github.com/blanu/Dust/go/cmd/DustProxy
+go install -tags privmodels
+~/go/bin/DustProxy --incomplete -c google.com:80 in ~/DustProxy.invite &
+echo "Connecting to $DUSTPROXYID"
+~/go/bin/DustProxy --incomplete -r 127.0.0.1 -l 127.0.0.1:16680 out 127.0.0.1:16667 $DUSTPROXYID m=http
+popd
