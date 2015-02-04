1fb018ea859913b2413608edc76a7ba1a90f3fca
Brandon Wiley <brandon@blanu.net>
2015-02-04 18:09:41 +0000

    Added script to build dependencies

---
+++ go-modelgen/prep.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get install mercurial
+pushd ~/go
+go get code.google.com/p/go.crypto/curve25519
+go get git.torproject.org/pluggable-transports/obfs4.git/common/csrand
+go get github.com/dchest/skein
+go get github.com/agl/ed25519/extra25519
+popd
