9f9b4d5c3d0ba037d297e65f4d5d8bd2b0c08d8c
Brandon Wiley <brandon@blanu.net>
2015-02-04 12:40:32 -0600

    Added some command line parameters

--- go-modelgen/scripts/run-only-client.sh
+++ go-modelgen/scripts/run-only-client.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "Connecting to $DUSTPROXYID"
-~/go/bin/DustProxy --incomplete -r 127.0.0.1 -l 127.0.0.1:16680 out $1:16667 $DUSTPROXYID m=$PROTOCOL
+echo "Connecting to $1 $2 $3"
+echo "Listening on localhost:16680"
+~/go/bin/DustProxy --incomplete -r 127.0.0.1 -l 127.0.0.1:16680 out $1:16667 $2 m=$3
