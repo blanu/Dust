273599ed8faf131ccc78e4f240aec14ac8176a8c
Brandon Wiley <brandon@blanu.net>
2015-05-19 13:06:39 -0500

    A script for building the privmodels

---
+++ modelgen/scripts/build-go.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+# compile.py [language] [modelDir] [packageName] [packageDir]
+python src/compile.py go ../go/models Dust_privmodels ~/go/src/caolta3iaejox3z4madc5wmp4z.uuid/Dust_privmodels
