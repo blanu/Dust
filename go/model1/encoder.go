117d6d47dad1134ac1ef26c6dd266327d15bc35e
Brandon Wiley <brandon@blanu.net>
2015-05-19 13:11:16 -0500

    Added max sleep parameter and commented out overrides for constant length and timing

--- go/model1/encoder.go
+++ go/model1/encoder.go
@@ -9,6 +9,7 @@ import (
 type Encoder struct {
 	SleepDist  Rand
 	LengthDist Rand
+	MaxSleep   time.Duration
 	Prefix     []byte
 	Unhuffer   *huffman.Decoder
 
@@ -33,11 +34,13 @@ func (enc *Encoder) NextPacketLength() uint16 {
 		result = clampUint16(enc.LengthDist.Rand())
 	}
 	return result
+	//	return 1440
 }
 
 func (enc *Encoder) NextPacketSleep() time.Duration {
 	// TODO: really clampUint16 here?
 	return time.Duration(clampUint16(enc.LengthDist.Rand())) * time.Millisecond
+	//	return 1 * time.Millisecond
 }
 
 func (enc *Encoder) ShapeBytes(dst, src []byte) (dn, sn int) {
